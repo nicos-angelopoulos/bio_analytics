3238d809657ace0012b6985e53e4d82d48e6b9c7
Nicos Angelopoulos <nicos@gmx.co.uk>
2023-06-07 16:39:36 +0100

    RETAB:  gene_family.pl

--- src/gene_family.pl
+++ src/gene_family.pl
@@ -48,48 +48,48 @@
 %
 gene_family( Family, OrgPrv, Symbols ) :-
     ( bio_db_organism(OrgPrv,Org) -> true; Org = OrgPrv ),
-	gene_family_known( Family, Org, Symbols ),
-	!,
-	debug( gene_family, 'Located family as the bio_analytics gene collection: ~w', [Family] ).
+     gene_family_known( Family, Org, Symbols ),
+     !,
+     debug( gene_family, 'Located family as the bio_analytics gene collection: ~w', [Family] ).
 gene_family( GoTerm, Org, Symbols ) :-
-	atomic( GoTerm ),
-	( integer(GoTerm) -> GoTerm = UpGoTerm; upcase_atom(GoTerm,UpGoTerm) ),
+     atomic( GoTerm ),
+     ( integer(GoTerm) -> GoTerm = UpGoTerm; upcase_atom(GoTerm,UpGoTerm) ),
     go_id( UpGoTerm, _GoAtom, GoInt ),
-	% atom_concat( 'GO:', GoAtmInt, UpGOTerm ),
+     % atom_concat( 'GO:', GoAtmInt, UpGOTerm ),
     % atom_number( GoAtmInt, GoInt ),
-	!,
+     !,
     % gene_family_org_go_term( Org, GoInt, Symbols ),
     go_symbols_reach( GoInt, Symbols, [organism(Org)] ),
-	debug( gene_family, 'Located GO term as the family identifier.', [] ).
+     debug( gene_family, 'Located GO term as the family identifier.', [] ).
 gene_family( Family, Org, Symbols ) :-
-	is_list( Family ),
-    maplist( is_symbol(Org), Family ),
-	% maplist( hgnc_homs_hgnc_symb, _, Family ),
-	!,
-	Symbols = Family,
-	debug( gene_family, 'Assertained input as list of symbols- passing it through.', [] ).
+     is_list( Family ),
+     maplist( is_symbol(Org), Family ),
+     % maplist( hgnc_homs_hgnc_symb, _, Family ),
+     !,
+     Symbols = Family,
+     debug( gene_family, 'Assertained input as list of symbols- passing it through.', [] ).
 gene_family( Family, Org, Symbols ) :-
-	is_list( Family ),
-    maplist( entz_symb(Org), Family, Symbols ),
-	!,
-	debug( gene_family, 'Converted input from Entrezes to Symbols.', [] ).
+     is_list( Family ),
+     maplist( entz_symb(Org), Family, Symbols ),
+     !,
+     debug( gene_family, 'Converted input from Entrezes to Symbols.', [] ).
 gene_family( Family, _Org, _Symbols ) :-
-	is_list( Family ),
-	!,
-	length( Family, Len ),
-	throw( gene_family(family_inpput_not_symbols_or_entrez_ids(length(Len))) ).
+     is_list( Family ),
+     !,
+     length( Family, Len ),
+     throw( gene_family(family_inpput_not_symbols_or_entrez_ids(length(Len))) ).
 gene_family( Family, Org, _Symbols ) :-
-	findall( Fam, gene_family_known(Fam,Org,_), Families ),
-	throw( gene_family(cannot_find_input_family_in_the_known_ones(Family,Org,Families)) ).
+     findall( Fam, gene_family_known(Fam,Org,_), Families ),
+     throw( gene_family(cannot_find_input_family_in_the_known_ones(Family,Org,Families)) ).
 
 /*
 gene_family_org_go_term( hs, GoInt, Symbols ) :-
     gene_family_org_go_term( human, GoInt, Symbols ).
 gene_family_org_go_term( human, GoInt, Symbols ) :-
-	findall( Symb, gont_homs_gont_symb(GoInt,_Evid,Symb), Symbols ).
+     findall( Symb, gont_homs_gont_symb(GoInt,_Evid,Symb), Symbols ).
 gene_family_org_go_term( mouse, GoInt, Symbols ) :-
-	findall( Symb, gont_musm_gont_symb(GoInt,_Evid,Symb), Symbols ).
+     findall( Symb, gont_musm_gont_symb(GoInt,_Evid,Symb), Symbols ).
     */
 
 gene_family_known( autophagy, hs, Auto ) :-
-	mtx_column( pack('bio_analytics/data/families/autophagy'), 'Symbol', Auto ).
+     mtx_column( pack('bio_analytics/data/families/autophagy'), 'Symbol', Auto ).
